57a83dec7888d1ea9e18a1631828c5e313f39f62
ikona1208 <73992486+ikona1208@users.noreply.github.com>
2025-08-03 20:25:02 +0900

    ESC-9, ESC-12, ESC-18対応（要修正）

--- lib/pages/setting_page/waiting_settings.dart
+++ lib/pages/setting_page/waiting_settings.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'widget/setting_section_widgets.dart';
 import 'dialogs/max_waiting_dialog.dart';
+import 'dialogs/estimated_waiting_dialog.dart';
 import '../../models/store_settings.dart';
 
 class WaitingSettings extends StatelessWidget {
   final StoreSettings storeSettings;
   final ValueChanged<StoreSettings> onChanged;
-  const WaitingSettings({super.key, required this.storeSettings, required this.onChanged});
+  const WaitingSettings(
+      {super.key, required this.storeSettings, required this.onChanged});
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +23,7 @@ class WaitingSettings extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 buildSettingItem(
-                  '最大待機',
+                  '最大待機人数',
                   '${storeSettings.waitingPolicy.maxWaitingCount}人',
                   null,
                   onTap: () {
@@ -31,6 +33,25 @@ class WaitingSettings extends StatelessWidget {
                       onConfirm: (value) {
                         final updated = storeSettings.copyWith(
                           waitingPolicy: storeSettings.waitingPolicy.copyWith(maxWaitingCount: value),
+                        );
+                        onChanged(updated);
+                      },
+                    );
+                  },
+                ),
+                buildSettingItem(
+                  '想定待機人数',
+                  // StoreSettings/WaitingPolicyにestimatedWaitingCountがある前提
+                  '${storeSettings.waitingPolicy.estimatedWaitingCount}人',
+                  null,
+                  onTap: () {
+                    // showEstimatedWaitingDialogは仮のダイアログ関数名
+                    showEstimatedWaitingDialog(
+                      context,
+                      storeSettings.waitingPolicy.estimatedWaitingCount ?? 0,
+                      onConfirm: (value) {
+                        final updated = storeSettings.copyWith(
+                          waitingPolicy: storeSettings.waitingPolicy.copyWith(estimatedWaitingCount: value),
                         );
                         onChanged(updated);
                       },
